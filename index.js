bbbc237825dbb069d30b108d6500e245d814e5c4
graceywambui <grace.wambui@student.moringaschool.com>
2022-11-18 18:44:06 +0300

    commit tree 5d9b7cf12766e464854f75053ed0b8025becc02f

---
+++ index.js
@@ -0,0 +1,11 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  function sayHelloTo(Grace) {
+    console.log(`hello, ${Grace}!`)
+  }
